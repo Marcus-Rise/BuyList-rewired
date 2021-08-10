22f5063877841106355cebfcec46a840f3f7e945
Marcus-Rise <ilykonst95@gmail.com>
2021-08-10 14:39:32 +0400

    refactor: user

--- src/server/user/service/user.service.ts
+++ src/server/user/service/user.service.ts
@@ -15,40 +15,46 @@ interface IAuth0ApiErrorResponse {
   error_description: string;
 }
 
-const isAuth0ApiErrorResponse = (
-  res: Record<string, unknown> | IAuth0ApiErrorResponse,
-): res is IAuth0ApiErrorResponse => {
-  return "error" in res && "error_description" in res;
-};
-
 class UserService implements IUserService {
-  async get(userId: string): Promise<UserModel> {
-    const AUTH0_TENANT = process.env.AUTH0_ISSUER_BASE_URL;
-    const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
-    const AUTH0_CLIENT_SECRET = process.env.AUTH0_CLIENT_SECRET;
-    const apiUrl = new URL("/api/v2/", AUTH0_TENANT);
+  constructor(
+    private readonly _tenant = process.env.AUTH0_ISSUER_BASE_URL,
+    private readonly _clientId = process.env.AUTH0_CLIENT_ID,
+    private readonly _clientSecret = process.env.AUTH0_CLIENT_SECRET,
+  ) {}
 
-    const { token_type, access_token } = await fetch(
-      new URL("/oauth/token", AUTH0_TENANT).toString(),
-      {
-        method: "POST",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify({
-          client_id: AUTH0_CLIENT_ID,
-          client_secret: AUTH0_CLIENT_SECRET,
-          audience: apiUrl,
-          grant_type: "client_credentials",
-        }),
-      },
-    ).then<IAuth0ApiTokenResponse>(async (res) => {
+  isAuth0ApiErrorResponse(
+    res: Record<string, unknown> | IAuth0ApiErrorResponse,
+  ): res is IAuth0ApiErrorResponse {
+    return "error" in res && "error_description" in res;
+  }
+
+  async getToken(): Promise<IAuth0ApiTokenResponse> {
+    const apiUrl = new URL("/api/v2/", this._tenant);
+
+    return fetch(new URL("/oauth/token", this._tenant).toString(), {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        client_id: this._clientId,
+        client_secret: this._clientSecret,
+        audience: apiUrl,
+        grant_type: "client_credentials",
+      }),
+    }).then<IAuth0ApiTokenResponse>(async (res) => {
       const data = await res.json();
 
-      if (isAuth0ApiErrorResponse(data)) {
+      if (this.isAuth0ApiErrorResponse(data)) {
         throw new UserException(res.status, data);
       }
 
       return data;
     });
+  }
+
+  async get(userId: string): Promise<UserModel> {
+    const apiUrl = new URL("/api/v2/", this._tenant);
+
+    const { token_type, access_token } = await this.getToken();
 
     const dto = await fetch(new URL(`${apiUrl}users/${userId}`).toString(), {
       method: "GET",
@@ -56,7 +62,7 @@ class UserService implements IUserService {
     }).then<IUserGetResponseDto>(async (res) => {
       const data = await res.json();
 
-      if (isAuth0ApiErrorResponse(data)) {
+      if (this.isAuth0ApiErrorResponse(data)) {
         throw new UserException(res.status, data);
       }
 
