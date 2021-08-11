a9bf2c3da62a8dba5668d77ebdfd786beadde0b5
Marcus-Rise <ilykonst95@gmail.com>
2021-08-11 13:00:22 +0400

    refactor(google)

--- src/server/google-drive/google-drive.service.ts
+++ src/server/google-drive/google-drive.service.ts
@@ -1,21 +1,10 @@
 import type { IGoogleDriveResponse, IGoogleDriveService } from "./google-drive.service.interface";
-import type { TokenInfo } from "google-auth-library";
 import { OAuth2Client } from "google-auth-library";
 import { GoogleDriveException } from "./google-drive.exception";
 import { google } from "googleapis";
 import type { UserProviderModel } from "../user";
 
 class GoogleDriveService implements IGoogleDriveService {
-  private readonly _oauthClient = new OAuth2Client();
-
-  async checkToken(accessToken: string): Promise<TokenInfo> {
-    return this._oauthClient.getTokenInfo(accessToken ?? "").catch((e) => {
-      const { status, data } = e?.response;
-
-      throw new GoogleDriveException(data || e, status);
-    });
-  }
-
   async createFile(
     name: string,
     mimeType: string,
@@ -27,19 +16,22 @@ class GoogleDriveService implements IGoogleDriveService {
       throw new GoogleDriveException("no provider refresh token", 400);
     }
 
-    await this.checkToken(accessToken).catch(({ code, message }) => {
-      throw new GoogleDriveException(message, code);
-    });
-
-    /*this._oauthClient.setCredentials({
+    const oauthClient = new OAuth2Client();
+    oauthClient.setCredentials({
       access_token: accessToken,
       refresh_token: refreshToken,
-    });*/
+    });
+    oauthClient.forceRefreshOnFailure = true;
+
+    await oauthClient.getTokenInfo(accessToken ?? "").catch((e) => {
+      const { status, data } = e?.response;
+
+      throw new GoogleDriveException(data || e, status);
+    });
 
     const drive = google.drive({
       version: "v3",
-      // auth: this._oauthClient,
-      headers: { Authorization: `Bearer ${accessToken}` },
+      auth: oauthClient,
     });
 
     return drive.files
@@ -53,8 +45,8 @@ class GoogleDriveService implements IGoogleDriveService {
         },
       })
       .then(({ status, data }) => ({ status, data }))
-      .catch((e) => {
-        throw new GoogleDriveException(e);
+      .catch(({ code, message }) => {
+        throw new GoogleDriveException(message, code);
       });
   }
 }
