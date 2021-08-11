330d7b02d2fc7cbe2d0d0b578cd95007d29407e4
Marcus-Rise <ilykonst95@gmail.com>
2021-08-11 10:00:26 +0400

    feat(user): catch empty refresh token

--- src/server/user/model/user-provider.model.factory.ts
+++ src/server/user/model/user-provider.model.factory.ts
@@ -1,8 +1,13 @@
 import { UserProviderModel } from "./user-provider.model";
 import type { IUserProviderGetResponseDto } from "../service/user-get-response.dto";
+import { UserException } from "../service";
 
 class UserProviderModelFactory {
   static fromGetResponseDto(dto: IUserProviderGetResponseDto): UserProviderModel {
+    if (!dto.refresh_token) {
+      throw new UserException("no provider refresh token", 400);
+    }
+
     return new UserProviderModel(dto.provider, dto.access_token, dto.refresh_token, dto.expires_in);
   }
 }
