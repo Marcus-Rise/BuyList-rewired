f9fb28b545040fd7c7a360ae0b1e5e2731cc7364
Marcus-Rise <ilykonst95@gmail.com>
2021-08-11 10:00:04 +0400

    feat(user): obtain refresh token

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,3 +1,17 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import { CONNECTION_SCOPE, SCOPES } from "../../../scopes";
 
-export default handleAuth();
+export default handleAuth({
+  async login(req, res) {
+    return handleLogin(req, res, {
+      authorizationParams: {
+        // audience: "https://www.googleapis.com", // or AUTH0_AUDIENCE
+        // Add the `offline_access` scope to also get a Refresh Token
+        access_type: "offline",
+        prompt: "consent",
+        scope: SCOPES.join(" "),
+        connection_scope: CONNECTION_SCOPE.join(" "),
+      },
+    }).catch((error) => res.status(error.status || 400).end(error.message));
+  },
+});
---
+++ scopes.ts
@@ -0,0 +1,8 @@
+const SCOPES = ["openid", "profile", "email", "offline_access"];
+const CONNECTION_SCOPE = [
+  "https://www.googleapis.com/auth/drive.file",
+  "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/userinfo.profile",
+];
+
+export { SCOPES, CONNECTION_SCOPE };
