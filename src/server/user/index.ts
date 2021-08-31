d668666395a612c19be1d88bcda5924e7aed4c8c
Marcus-Rise <ilykonst95@gmail.com>
2021-08-31 14:08:28 +0400

    fix

--- src/server/user/index.ts
+++ src/server/user/index.ts
@@ -2,4 +2,3 @@ export type { IUserService } from "./service";
 export { USER_SERVICE } from "./service";
 export * from "./model";
 export * from "./ioc";
-export * from "./user-json-storage.interceptor";
